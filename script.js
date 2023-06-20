78f5c9d31a5a56c944810666e9364942cc654792
john <50990111+johnedwardlee@users.noreply.github.com>
2023-06-19 22:30:20 -0400

    Random and erase implemented

--- script.js
+++ script.js
@@ -2,10 +2,25 @@
 let gridSize = 32;
 let mouseLock = false;
 let color = '#000000';
+let randLock = false;
+const grid = document.getElementById('grid');
+const gridStyle = getComputedStyle(grid);
+const colorBtn = document.querySelector('.btn-color');
+const randBtn = document.querySelector('.btn-rand');
+const shadeBtn = document.querySelector('.btn-shade');
+const gridSizeBtn = document.querySelector('.btn-size');
+const clearBtn = document.querySelector('.btn-clear');
+const eraserBtn = document.querySelector('.btn-eraser');
+const midButtonTexts = document.querySelectorAll('.front');
+const colorBox = document.getElementById('color-box');
 // Functions
 function fillBox(e) {
   if (e.type === 'mouseover' && !mouseLock) return;
-  e.target.style.backgroundColor = color;
+  if (randLock){
+    e.target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  } else {
+    e.target.style.backgroundColor = color;
+  }
 }
 
 function clearGrid() {
@@ -14,9 +29,9 @@ function clearGrid() {
 
 function drawGrid() {
   clearGrid();
-  let length = parseFloat(gridStyle.width) / gridSize;
-  for (let i = 0; i < gridSize ** 2; i++) {
-    let box = document.createElement('div');
+  const length = parseFloat(gridStyle.width) / gridSize;
+  for (let i = 0; i < gridSize ** 2; i += 1) {
+    const box = document.createElement('div');
     box.classList.add('grid-box');
     box.style.cssText = `width:${length}px;height:${length}px;`;
     box.addEventListener('mousedown', fillBox);
@@ -39,7 +54,7 @@ function setGridSize() {
 }
 
 function selectColor() {
-  let colorPicker = document.getElementById('color-picker');
+  const colorPicker = document.getElementById('color-picker');
   colorPicker.addEventListener('input', function () {
     color = this.value;
     colorBox.style.cssText = `background:${color}`;
@@ -47,21 +62,25 @@ function selectColor() {
   colorPicker.click();
 }
 
+function selectErase() {
+  color = '#ffff';
+}
+
+function selectRandom() {
+  if (!randLock) {
+    randLock = true;
+  } else {
+    randLock = false;
+  }
+}
+
 // Main
-const grid = document.getElementById('grid');
-const gridStyle = getComputedStyle(grid);
-const colorBtn = document.querySelector('.btn-color');
-const randBtn = document.querySelector('.btn-rand');
-const shadeBtn = document.querySelector('.btn-shade');
-const gridSizeBtn = document.querySelector('.btn-size');
-const clearBtn = document.querySelector('.btn-clear');
-const eraserBtn = document.querySelector('.btn-eraser');
-const midButtonTexts = document.querySelectorAll('.front');
-const colorBox = document.getElementById('color-box');
 midButtonTexts[2].textContent = `Grid Size: ${gridSize}`;
 gridSizeBtn.addEventListener('click', setGridSize);
 clearBtn.addEventListener('click', drawGrid);
 colorBtn.addEventListener('click', selectColor);
+eraserBtn.addEventListener('click', selectErase);
+randBtn.addEventListener('click', selectRandom);
 document.body.onmousedown = () => (mouseLock = true);
 document.body.onmouseup = () => (mouseLock = false);
 drawGrid();
